2b051d44c42a41f383b49933d75c27cfc179a375
Joakim Forsberg <joakim.forsberg1@gmail.com>
2011-12-06 19:11:03 +0100

    added more things to profile page also changed language tags to swedish

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Auth::Application.routes.draw do
   match "/authenticate", to: "authenticate#index"
   match "/auth/failure", to: "sessions#failure"
   match "/logout", to: "sessions#destroy", :as => "logout"
+  match "/profile", to: "profile#index"
   resources :identities
+
 end
