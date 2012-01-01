378f4a2ad0899c241059d74e252195d3807eb051
Joakim Forsberg <joakim.forsberg1@gmail.com>
2012-01-01 16:35:47 +0100

    Updated link from categories to tips

--- app/helpers/tips_helper.rb
+++ app/helpers/tips_helper.rb
@@ -2,7 +2,8 @@ module TipsHelper
 	def facebook_like
     	content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=standard&show_faces=true&width=450&action=like&font=arial&colorscheme=light&height=80", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
     end
-    def category_name(id)
-
+    def fetchCname(id)
+    	findc = Category.find(id)
+    	findc.categoryname
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Auth::Application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
-  resources :categories
+  
   match "/auth/:provider/callback", to: "sessions#create"
   match "/authenticate", to: "authenticate#index"
   match "ditt-tips-develop.dev", to: "home#index"
   match "/auth/failure", to: "sessions#failure"
   match "/tips/nytt",    to: "tips#new", as: "new_tips"
   match "/profil",    to: "profile#index", as: "profile"
-  match "/visar-alla-tips",    to: "browse#index", as: "browse"
+  match "/visar-alla-tips/:id",    to: "browse#index", as: "browse"
   match "/bli-medlem", to: "identities#new", as: "register"
   match "/visa-tips/:id", to: "showtip#index", as: "show_tip"
   match "/logout", to: "sessions#destroy", :as => "logout"
   match "/profile", to: "profile#index", :as => "user_profile"
+  match "/kategorier", to: "categories#index", :as => "cats"
+
+  resources :categories
   resources :identities
   resources :tips
   root to: "home#index"
