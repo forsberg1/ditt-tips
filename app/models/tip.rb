6a9c289e34907d5625758de171cb4110a9b69701
Joakim Forsberg <joakim.forsberg1@gmail.com>
2012-01-01 15:46:18 +0100

    Tii christian

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -2,6 +2,7 @@ class Tip < ActiveRecord::Base
 	attr_accessible :user_id, :title, :text, :image
 	mount_uploader :image, AvatarUploader
 	belongs_to              :user
+	belongs_to :category
 	validates_presence_of   :title
     validates_uniqueness_of :title
     validates_presence_of   :text
